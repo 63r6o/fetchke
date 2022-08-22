e37861ef344bb3dfe886544078f019e6e26c9c10
Gergo <landocarlissian@Gergos-MacBook-Air.local>
2022-08-22 17:23:28 +0200

    more idiomatic refactor

--- src/lib.rs
+++ src/lib.rs
@@ -37,3 +37,43 @@ impl DeviceData {
         }
     }
 }
+
+pub mod printer {
+    use ansi_term::Style;
+
+    pub fn print_rendered(rendered_info: &[(&str, &String)], decor: &String, title_style: &Style) {
+        for (i, item) in rendered_info.iter().enumerate() {
+            match i {
+                0 | 1 => println!("{}  {}", decor, item.1),
+                _ => {
+                    let line = format!("{}  {}: {}", decor, title_style.paint(item.0), item.1);
+                    println!("{}", line);
+                }
+            }
+        }
+    }
+}
+
+pub mod utils {
+    pub fn kb_to_gb(kb: u64) -> f64 {
+        (kb as f64) / 1048576.0
+    }
+
+    pub fn kb_to_mb(kb: u64) -> f64 {
+        (kb as f64) / 1024.0
+    }
+
+    pub fn sec_to_string(sec: u64) -> String {
+        let days = sec / (24 * 60 * 60);
+        let hours = (sec % (24 * 60 * 60)) / (60 * 60);
+        let minutes = (sec % (60 * 60)) / 60;
+
+        if hours < 1 {
+            format!("{minutes} minutes")
+        } else if days < 1 {
+            format!("{hours} hours, {minutes} minutes")
+        } else {
+            format!("{days} days, {hours} hours, {minutes} minutes")
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use ansi_term::{Colour, Style};
+use fetchke::{DeviceData, utils, printer};
 use whoami::Platform;
 
-mod device_data;
-mod utils;
-
 fn main() {
-    let device = device_data::DeviceData::new();
+    let device = DeviceData::new();
 
-    let rendered_info = [
+    let info_to_render = [
         ("Name", &device.name),
         ("Divider", &"-".repeat(device.name.len())),
         ("OS", &device.os),
@@ -39,18 +37,6 @@ fn main() {
     let decor = format!("{}", os_emoji.repeat(40));
 
     println!();
-    print_rendered(&rendered_info, &decor, &title_style);
+    printer::print_rendered(&info_to_render, &decor, &title_style);
     println!();
-}
-
-fn print_rendered(rendered_info: &[(&str, &String)], decor: &String, title_style: &Style) {
-    for (i, item) in rendered_info.iter().enumerate() {
-        match i {
-            0 | 1 => println!("{}  {}", decor, item.1),
-            _ => {
-                let line = format!("{}  {}: {}", decor, title_style.paint(item.0), item.1);
-                println!("{}", line);
-            }
-        }
-    }
 }
